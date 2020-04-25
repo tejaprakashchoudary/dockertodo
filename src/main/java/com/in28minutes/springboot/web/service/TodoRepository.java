05b700102260181d9620273438994654a644db6a
TejaPrakashChoudary <38122603+tejaprakashchoudary@users.noreply.github.com>
2020-04-25 15:14:36 +0530

    Initial Commit

--- src/main/java/com/in28minutes/springboot/web/service/TodoRepository.java
+++ src/main/java/com/in28minutes/springboot/web/service/TodoRepository.java
@@ -6,6 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.in28minutes.springboot.web.model.Todo;
 
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
 	List<Todo> findByUser(String user);
+	
+	//service.retrieveTodos(name)
+
+	//service.deleteTodo(id);
+	//service.retrieveTodo(id)
+	//service.updateTodo(todo)
+	//service.addTodo(getLoggedInUserName(model), todo.getDesc(), todo.getTargetDate(),false);
 }
