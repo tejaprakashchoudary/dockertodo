2eb0242b0ebf44182ee307bea8a96cee424898b6
TejaPrakashChoudary <38122603+tejaprakashchoudary@users.noreply.github.com>
2020-04-25 15:16:30 +0530

    Intial Commit with Mysql Data Base

--- src/main/java/com/in28minutes/springboot/web/model/Todo.java
+++ src/main/java/com/in28minutes/springboot/web/model/Todo.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.web.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -17,9 +18,11 @@ public class Todo {
 	private String user;
     
     @Size(min=10, message="Enter at least 10 Characters...")
+    @Column(name="description")
     private String desc;
 
     private Date targetDate;
+
     private boolean isDone;
 
     public Todo() {
